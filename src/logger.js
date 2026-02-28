cb1717658aa387b48a4663d3c3a4240188b6dae8
Chris Doten <chris@doten.us>
2026-02-28 00:49:35 -0500

    Added a smoke test, additional commands in package.json, some improvements to error logging, modifications to linting, and a /health check.

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,22 @@
 const pino = require('pino');
-
 const config = require('./config');
 
-module.exports = pino({
-  enabled: config.environment !== 'test',
-  level: process.env.LOG_LEVEL ?? 'error',
+const isTest = config.environment === 'test';
+const isProd = config.environment === 'production';
+
+const logger = pino({
+  enabled: !isTest,
+  level: process.env.LOG_LEVEL ?? (isProd ? 'error' : 'info'),
+  transport: !isProd
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+          ignore: 'pid,hostname',
+        },
+      }
+    : undefined,
 });
+
+module.exports = logger;
